22b8525ea1d7d3167d920025cc855f43323eca2c
Aishwarya Maheshwari <111682433+aish-w@users.noreply.github.com>
2025-02-28 07:29:06 +0000

    binary to decimal

---
+++ Array/Binary-to-Decimal.cpp
@@ -0,0 +1,20 @@
+// Input:-1000
+// Output:-8
+
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a=0,i=0;
+    while(n!=0){
+        int d=n%10;
+        if(d==1){
+            a=a+pow(2,i);
+        }
+        n=n/10;
+        i++;
+    }
+    cout<<a;
+}
