e3e5cc11d3fbaee69d15837417b4a16e9043f4c4
Aishwarya Maheshwari <111682433+aish-w@users.noreply.github.com>
2025-03-25 18:31:31 +0000

    oops

--- OOPS/Access modifiers.cpp
+++ OOPS/Access modifiers.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Hero{
     public: //we can access them anywhere outside or insisde of the class
     int health;
-    private: //we can access only inside class not out of the class
+    //private: //we can access only inside class not out of the class
     char level;
     void print(){
         cout<<level<<endl;
--- OOPS/Hero.cpp
+++ OOPS/Hero.cpp
@@ -1,5 +1,6 @@
 class Hero{
     //properties
+    public:
     int health;
     char level;
 };
