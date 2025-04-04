f48b940dee165522bdb3c01bf0828fbecbbc2dd9
Aishwarya Maheshwari <111682433+aish-w@users.noreply.github.com>
2025-04-04 07:14:36 +0000

    access modifier

--- OOPS/Access modifiers.cpp
+++ OOPS/Access modifiers.cpp
@@ -5,15 +5,32 @@ class Hero{
     int health;
     //private: //we can access only inside class not out of the class
     char level;
+    //protected : //we can not access outside the class and can be access in inherited class
     void print(){
         cout<<level<<endl;
+    }
+    int gethealth(){
+        return health;
+    }
+    char getlevel(){
+        return level;
+    }
+    void sethealth(int h){
+        health=h;
+    }
+    void setlevel(char ch){
+        level=ch;
     }
 };
 int main(){
     Hero h1;
-    h1.health=10;
+    //use getter function
+    cout<<h1.gethealth()<<endl;
+    //use setter function
+    h1.sethealth(70);
+    //h1.health=10;
     h1.level='A';
-    cout<<"Health: "<<h1.health<<endl;
+    cout<<"Health: "<<h1.gethealth()<<endl;
     cout<<"Level: "<<h1.level<<endl;
     return 0;
 }
